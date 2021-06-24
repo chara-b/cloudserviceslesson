51aa814c10d2ca025687abe9e6f2af6f81418469
chara-b <51973061+chara-b@users.noreply.github.com>
2021-06-24 16:53:21 +0300

    fixes on js file

--- handwritting.js
+++ handwritting.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
+const http = require('http');
 
 const handwritten = require('handwritten')
 const fs = require('fs')
@@ -34,24 +35,32 @@ app.post('/run', function (req, res) {
 
    var value = (req.body || {}).value;
    var payload = value.payload; 
-   if (typeof payload != 'string')
+   if (typeof payload != 'string'){
       payload = JSON.stringify(payload);
-
+   }
+   
+   const downloadedfile = fs.createWriteStream(meta["filename"]);
+   const request = http.get(payload, function(response) { // payload holds the presigned url inside it 
+      response.pipe(downloadedfile);
+   });
+/*
    // Make sure we got a filename on the command line.
    if (process.argv.length < 3) {
       console.log('Usage: node ' + process.argv[1] + ' FILENAME');
       process.exit(1);
    }
+   */
    // Read the file and print its contents. // index 0 is the node interpreter and index 1 is the script running on that nodeso index 2 is the actual parameter here 
-   filename = process.argv[2];
+ //  filename = process.argv[2];
+   filename = meta["filename"];
    fs.readFile(filename, 'utf8', function(err, file) {
      if (err) throw err;
      console.log('OK: ' + filename);
      //console.log(data)
-     handwritten(file).then((converted) => {
+     handwritten(file).then((convertedfile) => {
        //converted.pipe(fs.createWriteStream('output.pdf'))
        // Using fPutObject API upload your file to the bucket europetrip.
-    minioClient.fPutObject('handwritting', process.argv[1], converted, meta, function(err, etag) {
+    minioClient.fPutObject('handwritting', filename, convertedfile, meta, function(err, etag) {
       if (err) return console.log(err)
       console.log('File uploaded successfully.')
     });

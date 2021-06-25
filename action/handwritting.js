0ec5cd027147c83b11e295d14c99bdef657eb81c
chara-b <51973061+chara-b@users.noreply.github.com>
2021-06-25 17:03:21 +0300

    addd

--- action/handwritting.js
+++ action/handwritting.js
@@ -9,7 +9,7 @@ const fs = require('fs')
 var Minio = require('minio')
 
 var minioClient = new Minio.Client({
-   endPoint: '127.0.0.1',
+   endPoint: '172.17.0.1',
    port: 9000,
    useSSL: false,
    accessKey: 'minioadmin',
